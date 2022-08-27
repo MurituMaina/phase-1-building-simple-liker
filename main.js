a1800c5ed522d1df4efb62b6751f43bc95b5429f
MurituMaina <vincent.maina@student.moringaschool.com>
2022-08-27 18:53:06 +0300

    completed assignment

--- main.js
+++ main.js
@@ -2,7 +2,49 @@
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
+
 // Your JavaScript code goes here!
+//getting the the empty heart
+let liker = 
+document.getElementsByClassName('like-glyph')
+// console.log(liker)
+
+//add the call of the function
+// liker.addEventListener('click', liked);
+for(let like of liker){
+  like.addEventListener('click', liked)
+}
+
+function liked(e){
+  let emptyHeart = e.target;
+  mimicServerCall("https://lands.go.ke/")
+//manipulate DOM after server request responds
+    //display the error by removing the .hidden class
+  .then( (res) => {
+  alert(`${res}!!`)
+    if(emptyHeart.textContent === EMPTY_HEART ){
+      emptyHeart.className ="activated-heart";
+      emptyHeart.textContent = FULL_HEART 
+    }
+    //change heart back to empty heart
+    //remove the .activate-heart class
+
+    else if (emptyHeart.textContent === FULL_HEART){
+      emptyHeart.textContent = EMPTY_HEART;
+      emptyHeart.className = "";
+    }
+  })
+  //on serverfailure catch to handle the error
+    //display the error by removing the .hidden class
+  
+  .catch((error) => {
+    let getModal = document.getElementById('modal');
+    getModal.className="";
+    getModal.innerText = error;
+  })
+
+}
+//make heart full 
 
 
 
